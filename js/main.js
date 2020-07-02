0c44892f4cb834d3c8879fedc32d9646dd7c337e
yilverdeja <yilverdeja@gmail.com>
2020-07-03 00:03:33 +0800

    clean js and imgs

--- js/main.js
+++ js/main.js
@@ -8,13 +8,6 @@ function setupTooltips(){
     $('[data-toggle="tooltip"]').tooltip();
 }
 
-function changeImage(){
-    // figure out how to not repeat profile images that are currently being displayed
-    var rand = Math.floor(Math.random() * 3) + 1;
-    var newSrc = "images/profile/profile"+rand+".png";
-    $('#myImg').attr('src', newSrc);
-}
-
 function toggleExperience(num){
     if ($('.experience'+num).css('display') == 'none'){
         $('.experience'+num).show();
